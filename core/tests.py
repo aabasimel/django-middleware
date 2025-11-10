d58315387bebbe4f165b6cce991229c3445b5c95
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-11 00:51:57 +0100

    Add test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase,override_settings
+from django.test import Client
+from http import HTTPStatus
 
-# Create your tests here.
+
+class IPBlacklistMiddlewareTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    @override_settings(BANNED_IPS=None)
+    def test_request_successful_without_blacklist_settings(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,HTTPStatus.OK)
+    
+    @override_settings(BANNED_IPS=['192.168.1.2'])
+    def test_request_successful_with_non_blacklisted_ips(self):
+        response = self.client.get("/", REMOTE_ADDR='192.100.1.3')
+        self.assertEqual(response.status_code,HTTPStatus.OK)
+
+    @override_settings(BANNED_IPS =['192.168.1.2'])
+    def test_request_failed_with_blacklisted_ips(self):
+        response = self.client.get("/",REMOTE_ADDR='192.168.1.2')
+        self.assertEqual(response.status_code,HTTPStatus.FORBIDDEN)
+    
+
+
