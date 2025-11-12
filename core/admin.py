15665ad2f9d9a102aae59dcfc2b531e94e71072a
Ahmed Abasimel <abasimelahmed@gmail.com>
2025-11-12 16:00:07 +0100

    feat: Register new models in admin interface
    - Add Location and BlockedIP to Django admin
    - Configure display fields and search filters
    - Enable admin management of blocked IPs
    - Add location data visualization in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Location
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'latitude', 'longitude', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['user__username']
+    readonly_fields = ['created_at', 'updated_at']
